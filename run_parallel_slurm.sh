9d514001eb980081f0bb8875bf3369af1cfd4037
jaszczurgra <julian.mikolajczak@student.put.poznan.pl>
2026-01-02 22:17:29 +0100

    Added example maps

--- run_parallel_slurm.sh
+++ run_parallel_slurm.sh
@@ -11,8 +11,8 @@
 #SBATCH --time=0-0:30:59
 #SBATCH --array=0-4
 
-eval "$(/mnt/storage_3/project_data/pl0467-01/project_data/soft/miniconda3/bin/conda shell.bash hook)"
-conda activate scale_rl
+eval "$(/mnt/storage_6/project_data/pl0467-01/project_data/soft/miniconda3/bin/conda shell.bash hook)"
+conda activate planning_diffusion
 
 TASK_ID=${SLURM_ARRAY_TASK_ID}
 
