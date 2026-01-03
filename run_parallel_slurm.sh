b3d5cb627a21a495cc65a2fb8934a331b6205328
jaszczurgra <julian.mikolajczak@student.put.poznan.pl>
2026-01-03 03:28:00 +0100

    time setting in slurm

--- run_parallel_slurm.sh
+++ run_parallel_slurm.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=500
-#SBATCH --time=0-0:30:59
+#SBATCH --time=0-3:30:59
 #SBATCH --array=0-4
 
 eval "$(/mnt/storage_6/project_data/pl0467-01/soft/miniconda3/bin/conda shell.bash hook)"
