9a01974884e5981e73b40aacc129b0f1cf7d3d09
jaszczurgra <julian.mikolajczak@student.put.poznan.pl>
2026-01-03 01:08:27 +0100

    Bash based combinging

---
+++ combine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=MotionPlanningDatasetgenCombining
+#SBATCH --output=log/%j.out
+#SBATCH --error=log/%j.err
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=3000
+#SBATCH --time=0-0:30:59
+#SBATCH --array=0
+
+
+python src/combine_runs.py "$@"
--- run_combined_slurm.sh
+++ run_combined_slurm.sh
@@ -4,8 +4,7 @@
 JOB_ID=$(sbatch --parsable run_parallel_slurm.sh "$@")
 echo "Submitted array job: $JOB_ID"
 
-# Combining 
-POST_JOB_ID=$(sbatch --parsable --dependency=afterok:${JOB_ID} post_process.sh "$@")
+POST_JOB_ID=$(sbatch --parsable --dependency=afterok:${JOB_ID} combine.sh "$@")
 echo "Submitted post-processing job: $POST_JOB_ID (waits for $JOB_ID)"
 
 echo "Monitor jobs with: squeue -u $USER"
