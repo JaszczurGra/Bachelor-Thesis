8ba96210ce4a13e317b8b02716ff87b4d96396db
jaszczurgra <julian.mikolajczak@student.put.poznan.pl>
2026-01-21 03:09:02 +0100

    Visualizerof paths finished +-

--- src/visualizer.py
+++ src/visualizer.py
@@ -28,11 +28,12 @@ class Visualizer:
         self.font_size  = max(4, 12 -  n_cols) 
         n_rows = math.ceil(self.n_plots / n_cols)
         
-        self.fig, axs = plt.subplots(n_rows, n_cols)#, figsize=(5 * n_cols, 5 * n_rows))
+        self.fig, axs = plt.subplots(n_rows, n_cols,figsize=(5 * n_cols, 5 * n_rows))#, figsize=(5 * n_cols, 5 * n_rows))
         #TODO ADD the thtile back
         # self.fig.suptitle(f'OMPL Car Planning - Continuous', fontsize=16)
         # fig.set_facecolor('#2e2e2e')
         # Flatten axes
+        self.fig.tight_layout()
         if self.n_plots == 1:
             self.axs_flat = [axs]
         elif n_rows == 1 or n_cols == 1:
@@ -94,7 +95,6 @@ class Visualizer:
                 ax.text(1,0, legend_text, transform=ax.transAxes, 
                     verticalalignment='bottom',fontsize = self.font_size , horizontalalignment='right',
                     bbox=dict(boxstyle='round', facecolor='white', edgecolor='black', alpha=0.5))
-            print('drawn')
 
 
 
@@ -154,24 +154,27 @@ if __name__ == "__main__":
         exit(1)
 
 
-    map_folders = [d for d in os.listdir(args.data) if d.startswith('map_')]
+    map_folders = sorted(
+    [d for d in os.listdir(args.data) if d.startswith('map_')],
+    key=lambda x: int(x.split('_')[1])
+)
 
-    #TODO why does the multiple not show in saving 
+
+    all_results = []
+    map_idx = []
+    for map_folder in map_folders:
+        map_path = os.path.join(args.data, map_folder)
+        map_files = [f for f in os.listdir(map_path) if f == 'map.png']
+        map_array =  np.array(Image.open(os.path.join(map_path, map_files[0])).convert('1'))[::-1] if map_files else np.zeros((50,50)) 
+        path_files = [f for f in os.listdir(map_path) if f.startswith('path_') and f.endswith('.json')]
+        all_results += parse_path(map_path, path_files,map_array)
+        map_idx += [f"{map_folder}_path_{i}" for i in range(len(path_files))]
+    total_pages = math.ceil(len(all_results) / args.num_plots)
+
     if args.save is not None:
         os.makedirs(os.path.join('path_visualizations', args.save), exist_ok=True)
-        all_results = []
-        map_idx = []
-        for map_folder in map_folders:
-            map_path = os.path.join(args.data, map_folder)
-            map_files = [f for f in os.listdir(map_path) if f == 'map.png']
-            map_array =  np.array(Image.open(os.path.join(map_path, map_files[0])).convert('1'))[::-1] if map_files else np.zeros((50,50)) 
-            path_files = [f for f in os.listdir(map_path) if f.startswith('path_') and f.endswith('.json')]
-            all_results += parse_path(map_path, path_files,map_array)
-            map_idx += [f"{map_folder}_path_{i}" for i in range(len(path_files))]
 
-        visualizer = Visualizer(n_plots=1,show=False)
-        total_pages = math.ceil(math.sqrt(len(all_results) / args.num_plots))
-        visualizer.fig.tight_layout()
+        visualizer = Visualizer(n_plots=args.num_plots,show=False)
         for i in range(total_pages):
 
             start_idx = i * args.num_plots
@@ -182,41 +185,16 @@ if __name__ == "__main__":
             for plot_idx in range(args.num_plots):
                 result_idx = start_idx + plot_idx
                 if result_idx < end_idx:
-                    all_results[result_idx]['timestamp'] = i + 1 
+                    all_results[result_idx]['timestamp'] = datetime.now().timestamp() 
                     result_list[plot_idx] = all_results[result_idx]
-            print(result_list)
             visualizer.update(result_list, args.params,show_labels=False, custom_title=['']*args.num_plots)
-            print('saved')
             visualizer.fig.savefig(os.path.join('path_visualizations', args.save, f'visualization_{"-".join([map_idx[start_idx]] + ( [map_idx[end_idx-1]] if end_idx - 1 != start_idx else []) )}.pdf'))
+        exit(0)
 
-        exit(0)    
-
-    all_results = []
-    for map_folder in map_folders:
-        map_path = os.path.join(args.data, map_folder)
-        
-        # Load map image
-        map_files = [f for f in os.listdir(map_path) if f == 'map.png']
-
-        map_array =  np.array(Image.open(os.path.join(map_path, map_files[0])).convert('1'))[::-1] if map_files else np.zeros((50,50)) 
-
-
-        
-        # Load all path JSON files
-        path_files = [f for f in os.listdir(map_path) if f.startswith('path_') and f.endswith('.json')]
-        
-
-        all_results += parse_path(map_path, path_files,map_array)
-    
-    print(f"Loaded {len(all_results)} paths from {len(map_folders)} maps")
-    
-
-
-    visualizer = Visualizer(n_plots=args.num_plots)
+    visualizer = Visualizer(n_plots=args.num_plots,show= args.save is not None)
     current_page = 0 
-    total_pages = math.ceil(len(all_results) / args.num_plots)
 
-    def update_display():
+    def update_display(show_labels=True,custom_title=None):
         visualizer.fig.suptitle(f'OMPL Car Planning - Page {current_page + 1}/{total_pages} (Use ← → arrows)', fontsize=16)
 
         start_idx = current_page * args.num_plots
@@ -231,7 +209,10 @@ if __name__ == "__main__":
                 all_results[result_idx]['run'] = result_idx + 1
                 result_list[plot_idx] = all_results[result_idx]
         
-        visualizer.update(result_list, args.params)
+        visualizer.update(result_list, args.params, show_labels, custom_title=custom_title)
+        if args.save:
+            visualizer.fig.savefig(os.path.join('path_visualizations', args.save, f'visualization_{"-".join([map_idx[start_idx]] + ( [map_idx[end_idx-1]] if end_idx - 1 != start_idx else []) )}.pdf'))
+
         # print(f"Showing page {current_page + 1}/{total_pages} (results {start_idx + 1}-{end_idx} of {len(all_results)})")
     
 
@@ -247,8 +228,16 @@ if __name__ == "__main__":
                 current_page -= 1
                 update_display()
 
-    visualizer.fig.canvas.mpl_connect('key_press_event', on_key)
-    update_display()
+    if args.save is None:
+        visualizer.fig.canvas.mpl_connect('key_press_event', on_key)
+        update_display()
+    else:
+        os.makedirs(os.path.join('path_visualizations', args.save), exist_ok=True)
+        for i in range(total_pages):
+            current_page = i
+            update_display(show_labels=False, custom_title=['']*args.num_plots)
+        exit(0)
+
 
     plt.ioff()
     plt.show()
