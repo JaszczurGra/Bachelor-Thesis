1451617375602bc042eec31f9e7a189ee24b3db9
jaszczurgra <julian.mikolajczak@student.put.poznan.pl>
2026-01-23 00:44:23 +0100

    For some reason Genreatefolders is crashing but completing its job so runing paralelr gen even if the folder gen crashes

--- slurm_generate_dataset.sh
+++ slurm_generate_dataset.sh
@@ -22,7 +22,7 @@ echo "Running with animal name: $RANDOM_ANIMAL" and map folder: "$1"
 JOB_ID=$(sbatch --parsable slurm_generate_folders.sh "$RANDOM_ANIMAL" "$1" "$N" "$R")
 echo "Submitted folder generation job: $JOB_ID"
 
-POST_JOB_ID=$(sbatch --parsable --dependency=afterok:${JOB_ID} run_parallel_slurm.sh "$RANDOM_ANIMAL" "$1" "$N" "$R")
+POST_JOB_ID=$(sbatch --parsable --dependency=afterany:${JOB_ID} run_parallel_slurm.sh "$RANDOM_ANIMAL" "$1" "$N" "$R")
 echo "Submitted worker jobs: $POST_JOB_ID (waits for $JOB_ID)"
 
 echo "Monitor jobs with: squeue -u $USER"
