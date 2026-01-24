d041ec566f297c7f4ce93b883bf3af12854de906
JaszczurGra <julian.mikolajczak@student.put.poznan.pl>
2026-01-24 18:21:55 +0100

    Removed unnecesary vis

--- slurm_vis_all.sh
+++ slurm_vis_all.sh
@@ -1,8 +1,3 @@
-
-
-sbatch slurm_vis_launch.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/5zjjjyz2/runs/cqh7b021" \
-
-
 JOB_ID=$(sbatch --parsable slurm_generate_folders.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/xrqywefs/table?nw=nwuserjaszczurgra" 512 "")
 echo "Submitted sweep vis: $JOB_ID"
 JOB_ID=$(sbatch --parsable slurm_generate_folders.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/v7xcqp8a/table?nw=nwuserjaszczurgra" 16 "")
