5ac6ed38cb8e1e502d612410f9727f42718b9ba8
JaszczurGra <julian.mikolajczak@student.put.poznan.pl>
2026-01-24 18:24:51 +0100

    Launch file misname fix

--- slurm_vis_all.sh
+++ slurm_vis_all.sh
@@ -1,8 +1,8 @@
-JOB_ID=$(sbatch --parsable slurm_generate_folders.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/xrqywefs/table?nw=nwuserjaszczurgra" 512 "")
+JOB_ID=$(sbatch --parsable slurm_vis_launch.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/xrqywefs/table?nw=nwuserjaszczurgra" 512 "")
 echo "Submitted sweep vis: $JOB_ID"
-JOB_ID=$(sbatch --parsable slurm_generate_folders.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/v7xcqp8a/table?nw=nwuserjaszczurgra" 16 "")
+JOB_ID=$(sbatch --parsable slurm_vis_launch.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/v7xcqp8a/table?nw=nwuserjaszczurgra" 16 "")
 echo "Submitted sweep vis: $JOB_ID"
-JOB_ID=$(sbatch --parsable slurm_generate_folders.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/xrqywefs/table?nw=nwuserjaszczurgra" 512 "data/grouchy_penguin_23-01-2026_00:43:35")
+JOB_ID=$(sbatch --parsable slurm_vis_launch.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/xrqywefs/table?nw=nwuserjaszczurgra" 512 "data/grouchy_penguin_23-01-2026_00:43:35")
 echo "Submitted sweep vis: $JOB_ID"
-JOB_ID=$(sbatch --parsable slurm_generate_folders.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/v7xcqp8a/table?nw=nwuserjaszczurgra" 16 "data/grouchy_penguin_23-01-2026_00:43:35")
+JOB_ID=$(sbatch --parsable slurm_vis_launch.sh "https://wandb.ai/j-boro-poznan-university-of-technology/Bachelor-Thesis-src_model/sweeps/v7xcqp8a/table?nw=nwuserjaszczurgra" 16 "data/grouchy_penguin_23-01-2026_00:43:35")
 echo "Submitted sweep vis: $JOB_ID"
